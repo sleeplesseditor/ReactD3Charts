7c4949d0bcb32b56730186a48fd0d579cb6d595e
Josh Bryson <josh@bryson.me.uk>
2019-07-15 23:48:29 +0100

    Responsive navbar created

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
 import './styles.css';
+import Header from './components/Header/Header';
 import PieChartPage from './pages/PieChartPage';
 import BarChartPage from './pages/BarChartPage';
 import ChordPage from './pages/ChordPage';
@@ -9,31 +10,8 @@ function App() {
 
   return (
     <Router>
-      <div className="navbar">
-          <Link 
-            to={"/"}
-            style={{ textDecoration: 'none' }}
-          >
-            <p className="nav_header">React D3 Charts</p>
-          </Link>
-          <ul className="navbar-links">
-              <li>
-                  <Link to={"/"}>
-                      <p className="nav_header">Pie Chart</p>
-                  </Link>
-              </li>
-              <li>
-                  <Link to={"/barchart"}>
-                      <p className="nav_header">Bar Chart</p>
-                  </Link>
-              </li>
-              <li>
-                  <Link to={"/chordchart"}>
-                      <p className="nav_header">Chord Chart</p>
-                  </Link>
-              </li>
-          </ul>
-      </div>
+      <Header />
+        <br />
         <Switch>
           <Route exact path="/" component={PieChartPage} />
           <Route path="/barchart" component={BarChartPage} />
---
+++ src/components/Header/Header.js
@@ -0,0 +1,75 @@
+/* eslint-disable jsx-a11y/accessible-emoji */
+import React, { useState, useEffect } from "react";
+import "./Header.css";
+import HeaderIcon from './img/baseline_menu_white_18dp.png';
+import { Link } from 'react-router-dom';
+import { CSSTransition } from "react-transition-group";
+
+export default function Header() {
+  const [isNavVisible, setNavVisibility] = useState(false);
+  const [isSmallScreen, setIsSmallScreen] = useState(false);
+
+  useEffect(() => {
+    const mediaQuery = window.matchMedia("(max-width: 1500px)");
+    mediaQuery.addListener(handleMediaQueryChange);
+    handleMediaQueryChange(mediaQuery);
+
+    return () => {
+      mediaQuery.removeListener(handleMediaQueryChange);
+    };
+  }, []);
+
+  const handleMediaQueryChange = mediaQuery => {
+    if (mediaQuery.matches) {
+      setIsSmallScreen(true);
+    } else {
+      setIsSmallScreen(false);
+    }
+  };
+
+  const toggleNav = () => {
+    setNavVisibility(!isNavVisible);
+  };
+
+  return (
+    <header className="Header">
+        <h3 className="navbar-heading">React D3 Charts</h3>
+      <CSSTransition
+        in={!isSmallScreen || isNavVisible}
+        timeout={350}
+        classNames="NavAnimation"
+        unmountOnExit
+      >
+        <nav className="Nav">
+            <Link 
+                to={"/"}
+                style={{ textDecoration: 'none' }}
+                onClick={toggleNav}
+            >
+                Pie Chart
+            </Link>
+            <Link 
+                to={"/barchart"}
+                style={{ textDecoration: 'none' }}
+                onClick={toggleNav}
+            >
+                Bar Chart
+            </Link>
+            <Link 
+                to={"/chordchart"}
+                style={{ textDecoration: 'none' }}
+                onClick={toggleNav}
+            >
+                Chord Chart
+            </Link>
+        </nav>
+      </CSSTransition>
+      <button onClick={toggleNav} className="header-button">
+        <img 
+            src={HeaderIcon} 
+            alt="HeaderIcon"
+        />
+      </button>
+    </header>
+  );
+}
